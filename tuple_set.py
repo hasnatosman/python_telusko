612728c1161ea8d76c286262e950a988fcd03079
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-01-22 23:53:11 +0600

    Add files via upload

---
+++ tuple_set.py
@@ -0,0 +1,16 @@
+"""
+Tuple is almost like list except it is immutable. .....
+"""
+
+tup = (4, 5, 8)
+print(tup)
+
+"""
+When there is no needs of mutation then we can use tup.
+Iteration is faster in tuple than list. that is why we should use tuple .
+"""
+
+# set......................
+
+s = {5, 6, 3, 5, 8, 6, 2, 5, 8}
+print(s)
