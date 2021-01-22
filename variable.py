55abf2fa08cb99ad66365570ebc637ccae61afc1
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-01-22 07:19:56 +0600

    Add files via upload
    here, we briefly discussed the variable and usages it.

---
+++ variable.py
@@ -0,0 +1,23 @@
+# Variable is a container where you can put value.
+x = 5
+y = 6
+
+"""
+Here,
+x, y are variable and 5, 6 are values. Value can be changed whenever we want.
+
+"""
+z = x + y
+
+firstName = 'Hasnat'
+
+lastName = 'Osman'
+fullName = lastName + ' ' + firstName
+age = 25
+print(firstName)
+print(firstName[0:6:2], lastName[2:])
+
+
+print('My name is ' + ' ' + fullName + ' & my age is' + ' ' + str(age))
+
+print('The result is: ', z)
