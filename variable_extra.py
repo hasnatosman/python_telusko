ef64e8a4d0e8cceb8e3c29d4d5dd8e2670ee2902
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-01-24 09:56:07 +0600

    Add files via upload

---
+++ variable_extra.py
@@ -0,0 +1,14 @@
+x = 22
+x1 = 22
+y = 'Hasnat'
+y1 = 'Osman'
+list = [2, 4]
+tup = (2, 4)
+dict = {1: 'has', 2: 'os'}
+print(id(x))
+print(id(x1))
+print(id(y))
+print(id(y1))
+print(id(list))
+print(id(tup))
+print(id(dict))
