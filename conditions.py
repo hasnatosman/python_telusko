c4a5e7e0f3647bd05cd90df6d916e7075dfdb638
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-02-01 16:00:13 +0600

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,9 @@
+x = int(input('Input a number: '))
+if x < 0:
+    print('It is negative number.')
+elif x == 0:
+    print('It is a Big zero.')
+elif x >= 1:
+    print('It is a positive number.')
+else:
+    print('So what is this ?')
