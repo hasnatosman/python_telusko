81d78ac3f2ef5a5818c13085717808e59558e77c
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-02-08 15:39:08 +0600

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,11 @@
+has = {
+    'NAME': 'Hasnat',
+    'ID': 11222,
+    'YEAR': 2012
+}
+
+for i, j in has.items():
+    print(i, j)
+
+for i, j in enumerate(['Hasnat', 'Osman', 'Khan']):
+    print(i, j)
