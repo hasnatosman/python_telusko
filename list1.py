2b4e39e1463a16f68a4b361dc82add6be3b58056
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-02-14 12:41:14 +0600

    Add files via upload

--- list1.py
+++ list1.py
@@ -1,2 +1,10 @@
-fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
-print(fruits)
+lst = [1, 2, 3, 4, 5]
+
+new_list = []
+
+for i in lst:
+    print()
+    new_list.append(i ** 2)
+
+
+print(lst, new_list)
